428b54b4867989e94ca9c785806fe48f05e3a1a4
Gokul Nadathur <gokul.nadathur@gmail.com>
2016-06-29 18:06:12 -0700

    Simpler algo.

--- validateBST.cpp
+++ validateBST.cpp
@@ -9,57 +9,20 @@
  */
 class Solution {
 public:
-    inline bool isLeaf(TreeNode* n) {
-        return n->left == NULL && n->right == NULL;
-    }
-    bool isValidBSTInternal(TreeNode* root, int& minVal, int& maxVal)
+    bool isValidBSTInternal(TreeNode* root, int minVal, int maxVal)
     {
-        int currMin, currMax;
-
         if (root == NULL) {
             return true;
         }
-        if (isLeaf(root)) {
-            minVal = maxVal = root->val;
-            return true;
-        }
 
-        if (root->left) {
-            if (!isValidBSTInternal(root->left, minVal, maxVal)) {
-                return false;
-            }
-
-            if (root->val <= maxVal) {
-                return false;
-            }
-
-
-            currMin = min(root->val, minVal);
-            currMax = max(root->val, maxVal);
-        } else {
-            currMin = currMax = root->val;
-        }
-
-        if (root->right) {
-            if (!isValidBSTInternal(root->right, minVal, maxVal)) {
-                return false;
-            }
-            if (root->val >= minVal) {
-                return false;
-            }
-            minVal = min(minVal, currMin);
-            maxVal = max(maxVal, currMax);
-        } else {
-            minVal = currMin;
-            maxVal = currMax;
-        }
-
-
-        return true;
+	return (root->val > minVal &&
+		root->val < maxVal &&
+		isValidBSTInternal(root->left, minVal, root->val) &&
+		isValidBSTInternal(root->right, root->val, maxVal));
     }
 
     bool isValidBST(TreeNode* root) {
         int minVal, maxVal;
-        return isValidBSTInternal(root, minVal, maxVal);
+        return isValidBSTInternal(root, LONG_MIN, LONG_MAX);
     }
 };
